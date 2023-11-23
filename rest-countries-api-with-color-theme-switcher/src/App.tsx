7d3cf107f830e647eeeb2b13c2bbf973ab6b05d9
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-11-23 13:05:53 +0800

    parent component responsible for rendering all child components, imported react-router

---
+++ rest-countries-api-with-color-theme-switcher/src/App.tsx
@@ -0,0 +1,84 @@
+
+import {BrowserRouter as Router, Routes, Route,  } from 'react-router-dom';
+import React, {useState, useEffect} from "react";
+import NavBar from "./components/navBar";
+import './App.css';
+import SearchBar from "./components/searchbar";
+import ToggleButton from "./components/button";
+import DropDown from "./components/dropDown";
+import CountryList from "./components/countriesList";
+import CountryDetails from "./components/countriesDetails";
+
+
+
+
+
+function App () {
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+    const [selectedRegion, setSelectedRegion] = useState<string | null>(null);
+    const [searchCountry, setSearchCountry] = useState<string>('');
+   
+    
+
+ 
+
+    useEffect(() => {
+      document.body.className = isDarkMode ? 'dark-theme' : 'light-theme';
+    }, [isDarkMode]);
+
+    const toggleTheme  = () => {
+      setIsDarkMode(!isDarkMode);
+    };
+
+    const handleRegionSelect = (region: string | null) => {
+      setSelectedRegion(region);
+    };   
+
+  return (
+    <div className={`App ${isDarkMode ? 'dark' : 'light'}`}> 
+    <div className='header'>
+    <div className="navigate"><NavBar/></div>
+    <div className="click-button"><ToggleButton isDarkMode={isDarkMode} onToggle={toggleTheme}/></div> 
+    </div>
+
+    <div  className="flex">
+    
+       
+      <div>
+     <SearchBar isDarkMode={isDarkMode} onSearch={setSearchCountry}/>
+      </div>
+    <div className="dropdown-flex">
+     <DropDown isDarkMode={isDarkMode}  onRegionSelect={handleRegionSelect} />
+      </div>
+     
+    
+    </div>
+    <div>
+   
+    <Router>
+      <Routes>
+        <Route path='/' element={<CountryList isDarkMode={isDarkMode} selectedRegion={selectedRegion} searchCountry={searchCountry} />} />
+        <Route  path='/country/:countryName' element={<CountryDetails  isDarkmode />}/>
+      </Routes>
+      </Router>
+       
+ 
+     
+ 
+      
+  
+    
+       
+    
+    </div>
+     </div>
+     
+    
+   
+
+  
+   
+  );
+}
+
+export default App;
