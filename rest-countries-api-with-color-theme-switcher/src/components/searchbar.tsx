8437256a1a4c40009a6e09b6fc6d17ce847fe05b
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-11-23 13:27:41 +0800

    component for handling country search

---
+++ rest-countries-api-with-color-theme-switcher/src/components/searchbar.tsx
@@ -0,0 +1,47 @@
+import React, {FC,ChangeEvent } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+
+
+
+
+interface SearchBarProps{
+    isDarkMode: boolean
+    onSearch: (searchCountry: string) => void;
+
+    
+
+   
+   
+}
+
+const SearchBar: FC<SearchBarProps> = ({isDarkMode, onSearch}) => {
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const searchCountry = event.target.value;
+        onSearch(searchCountry);
+    
+    }
+
+
+
+    return (
+        <div className={` ${isDarkMode ? 'dark' : 'light'}`}>
+        <div className='input-container' >
+            <input type="text" className="countrySearch" placeholder="Search for a country..."  onChange={handleInputChange}/>
+<br/>
+           <div className='icon-container'>
+            <FontAwesomeIcon className='searchicon' icon = {faMagnifyingGlass} size='xs'></FontAwesomeIcon>
+            </div>
+          
+        </div>
+        </div>
+                     
+      
+        
+
+
+        
+)
+
+}
+export default SearchBar;
