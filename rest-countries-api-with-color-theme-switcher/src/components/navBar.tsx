3d68c7173cb0ba26298c825227b70a993ec584ef
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-11-23 13:25:34 +0800

    navbar component built

---
+++ rest-countries-api-with-color-theme-switcher/src/components/navBar.tsx
@@ -0,0 +1,20 @@
+import React, {FC} from "react";
+
+
+const NavBar:FC = () => {
+
+    return(
+        <div>
+          <p className='word'>
+            Where in the world?
+            </p>
+           
+        </div>
+     
+        
+    );
+}
+
+
+
+export default NavBar;
