e248bde2fc27df6f8e2185d1f3423e5fc4360a76
Dev-Kwame <kwame.agyabeng@amalitech.org>
2023-12-01 23:58:42 +0800

    included countries props to component

--- rest-countries-api-with-color-theme-switcher/src/components/countriesList.tsx
+++ rest-countries-api-with-color-theme-switcher/src/components/countriesList.tsx
@@ -1,84 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Country } from '../App';
 
 
 
 
- export interface Country {
-    name: {
-    common: string;
-    nativeName: {
-      [key: string]: {
-        official: string;
-        common: string;
-      };
-    };
-   
-  };
 
-        flags: {
-            svg: string;
-            png: string;
-          };
+
+
        
-        population: number;
-        region: string;
-        capital: string;
-        subregion: string;
-        borders: string[];
-        tld: string;
-        languages: {
-          [key: string]: string;
-        };
-        currencies: {
-          [key: string]: {
-            name: string;
-          
-          };
-        };
-       cca3: string;
-      
- }
-       
-      
-        
-        
 
-   
 
       interface CountryListProps {
         isDarkMode: boolean;
         selectedRegion: string | null;
         searchCountry: string;
-      
-        
+        countries: Country[];
       
       }
+  
+
+
+      
       
      
       
-  const CountryList: React.FC<CountryListProps> = ({isDarkMode, selectedRegion,searchCountry}) => {
-    const [countries, setCountries] = useState<Country[]>([]);
-
-   
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://restcountries.com/v3.1/all');
-        const data = await response.json();
-        console.log('Data from API:', data);
-        setCountries(data);
-
-        
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        
-
-      }
-    };
-
-    fetchData();
-  }, []);
+  const CountryList: React.FC<CountryListProps> = ({isDarkMode, selectedRegion,searchCountry, countries}) => {
+    
+ 
   
   const filteredCountries = countries.filter((country) => {
     const matchesRegion = selectedRegion && selectedRegion !== 'all'
@@ -102,21 +51,22 @@ import { Link } from 'react-router-dom';
       <ul className='country-grid'>
      
             {filteredCountries.map((country) =>(
-              
-              <Link className='text' key={country.name.common} to={{
-                pathname: `/country/${encodeURIComponent(country.name.common)}`
-              }} >
-              <div key={country.capital }className={`countryItem ${isDarkMode ? 'dark' : 'light'}`}>
+            <Link className='text' key={country.name.common} to={`/country/${encodeURIComponent(country.name.common)}` } 
+       > 
+              <div key={country.name.common }className={`countryItem ${isDarkMode ? 'dark' : 'light'}`}  >
             <img src={country.flags.png} alt="Flag" className='countryImg'/>
              <li id='country-name'>{country.name.common}</li>
               <li><span>Population: </span>{country.population.toLocaleString()}</li>
               <li><span>Region:</span> {country.region}</li>
               <li><span>Capital: </span>{country.capital}</li>  
+
+            
               
             </div> 
            </Link>
         ))}
       </ul>
+    
       </div>
    
   );
